45970909578d7849ec0949ac5c659eb6c559b753
Nadezhda73 <nprokhorova077@gmail.com>
2020-01-23 18:42:12 +0400

    Lab1

---
+++ WindowsFormsMonorail/WindowsFormsMonorail/FormMonorail.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace WindowsFormsMonorail
+{
+    partial class FormMonorail
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxMonorail = new System.Windows.Forms.PictureBox();
+            this.buttonCreate = new System.Windows.Forms.Button();
+            this.buttonUp = new System.Windows.Forms.Button();
+            this.buttonRight = new System.Windows.Forms.Button();
+            this.buttonDown = new System.Windows.Forms.Button();
+            this.buttonLeft = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMonorail)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxMonorail
+            // 
+            this.pictureBoxMonorail.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxMonorail.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxMonorail.Name = "pictureBoxMonorail";
+            this.pictureBoxMonorail.Size = new System.Drawing.Size(884, 461);
+            this.pictureBoxMonorail.TabIndex = 0;
+            this.pictureBoxMonorail.TabStop = false;
+            // 
+            // buttonCreate
+            // 
+            this.buttonCreate.Location = new System.Drawing.Point(30, 25);
+            this.buttonCreate.Name = "buttonCreate";
+            this.buttonCreate.Size = new System.Drawing.Size(75, 23);
+            this.buttonCreate.TabIndex = 1;
+            this.buttonCreate.Text = "Создать";
+            this.buttonCreate.UseVisualStyleBackColor = true;
+            this.buttonCreate.Click += new System.EventHandler(this.ButtonCreate_Click);
+            // 
+            // buttonUp
+            // 
+            
+            this.buttonUp.Location = new System.Drawing.Point(760, 353);
+            this.buttonUp.Name = "buttonUp";
+            this.buttonUp.Size = new System.Drawing.Size(30, 30);
+            this.buttonUp.TabIndex = 2;
+            this.buttonUp.UseVisualStyleBackColor = true;
+            this.buttonUp.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonRight
+            // 
+            
+            this.buttonRight.Location = new System.Drawing.Point(796, 389);
+            this.buttonRight.Name = "buttonRight";
+            this.buttonRight.Size = new System.Drawing.Size(30, 30);
+            this.buttonRight.TabIndex = 3;
+            this.buttonRight.UseVisualStyleBackColor = true;
+            this.buttonRight.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonDown
+            // 
+            
+            this.buttonDown.Location = new System.Drawing.Point(760, 389);
+            this.buttonDown.Name = "buttonDown";
+            this.buttonDown.Size = new System.Drawing.Size(30, 30);
+            this.buttonDown.TabIndex = 4;
+            this.buttonDown.UseVisualStyleBackColor = true;
+            this.buttonDown.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonLeft
+            // 
+    
+            this.buttonLeft.Location = new System.Drawing.Point(724, 389);
+            this.buttonLeft.Name = "buttonLeft";
+            this.buttonLeft.Size = new System.Drawing.Size(30, 30);
+            this.buttonLeft.TabIndex = 5;
+            this.buttonLeft.UseVisualStyleBackColor = true;
+            this.buttonLeft.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // FormMonorail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 461);
+            this.Controls.Add(this.buttonLeft);
+            this.Controls.Add(this.buttonDown);
+            this.Controls.Add(this.buttonRight);
+            this.Controls.Add(this.buttonUp);
+            this.Controls.Add(this.buttonCreate);
+            this.Controls.Add(this.pictureBoxMonorail);
+            this.Name = "FormMonorail";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Монорельс";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMonorail)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxMonorail;
+        private System.Windows.Forms.Button buttonCreate;
+        private System.Windows.Forms.Button buttonUp;
+        private System.Windows.Forms.Button buttonRight;
+        private System.Windows.Forms.Button buttonDown;
+        private System.Windows.Forms.Button buttonLeft;
+    }
+}
+
