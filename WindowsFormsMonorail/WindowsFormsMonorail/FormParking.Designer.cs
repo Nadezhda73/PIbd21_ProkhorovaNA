90b1b069fa45e64cee11a79a0c931e41183e7f27
Nadezhda73 <nprokhorova077@gmail.com>
2020-01-23 19:41:58 +0400

    Lab4

--- WindowsFormsMonorail/WindowsFormsMonorail/FormParking.Designer.cs
+++ WindowsFormsMonorail/WindowsFormsMonorail/FormParking.Designer.cs
@@ -36,6 +36,8 @@
             this.buttonTake = new System.Windows.Forms.Button();
             this.maskedTextBoxParking = new System.Windows.Forms.MaskedTextBox();
             this.labelParking = new System.Windows.Forms.Label();
+            this.listBoxLevels = new System.Windows.Forms.ListBox();
+            this.labelLevels = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxParking)).BeginInit();
             this.groupBoxParking.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTake)).BeginInit();
@@ -45,13 +47,13 @@
             // 
             this.pictureBoxParking.Location = new System.Drawing.Point(12, 12);
             this.pictureBoxParking.Name = "pictureBoxParking";
-            this.pictureBoxParking.Size = new System.Drawing.Size(754, 426);
+            this.pictureBoxParking.Size = new System.Drawing.Size(762, 426);
             this.pictureBoxParking.TabIndex = 0;
             this.pictureBoxParking.TabStop = false;
             // 
             // ParkingMonorail
             // 
-            this.ParkingMonorail.Location = new System.Drawing.Point(879, 12);
+            this.ParkingMonorail.Location = new System.Drawing.Point(879, 169);
             this.ParkingMonorail.Name = "ParkingMonorail";
             this.ParkingMonorail.Size = new System.Drawing.Size(93, 44);
             this.ParkingMonorail.TabIndex = 1;
@@ -61,7 +63,7 @@
             // 
             // ParkingLocomotive
             // 
-            this.ParkingLocomotive.Location = new System.Drawing.Point(879, 73);
+            this.ParkingLocomotive.Location = new System.Drawing.Point(780, 169);
             this.ParkingLocomotive.Name = "ParkingLocomotive";
             this.ParkingLocomotive.Size = new System.Drawing.Size(93, 44);
             this.ParkingLocomotive.TabIndex = 2;
@@ -75,9 +77,9 @@
             this.groupBoxParking.Controls.Add(this.buttonTake);
             this.groupBoxParking.Controls.Add(this.maskedTextBoxParking);
             this.groupBoxParking.Controls.Add(this.labelParking);
-            this.groupBoxParking.Location = new System.Drawing.Point(772, 197);
+            this.groupBoxParking.Location = new System.Drawing.Point(780, 219);
             this.groupBoxParking.Name = "groupBoxParking";
-            this.groupBoxParking.Size = new System.Drawing.Size(200, 241);
+            this.groupBoxParking.Size = new System.Drawing.Size(200, 219);
             this.groupBoxParking.TabIndex = 3;
             this.groupBoxParking.TabStop = false;
             this.groupBoxParking.Text = "Забрать машину";
@@ -86,7 +88,7 @@
             // 
             this.pictureBoxTake.Location = new System.Drawing.Point(22, 121);
             this.pictureBoxTake.Name = "pictureBoxTake";
-            this.pictureBoxTake.Size = new System.Drawing.Size(160, 104);
+            this.pictureBoxTake.Size = new System.Drawing.Size(160, 92);
             this.pictureBoxTake.TabIndex = 3;
             this.pictureBoxTake.TabStop = false;
             // 
@@ -116,22 +118,43 @@
             this.labelParking.TabIndex = 0;
             this.labelParking.Text = "Место:";
             // 
+            // listBoxLevels
+            // 
+            this.listBoxLevels.FormattingEnabled = true;
+            this.listBoxLevels.Location = new System.Drawing.Point(790, 28);
+            this.listBoxLevels.Name = "listBoxLevels";
+            this.listBoxLevels.Size = new System.Drawing.Size(172, 121);
+            this.listBoxLevels.TabIndex = 4;
+            this.listBoxLevels.SelectedIndexChanged += new System.EventHandler(this.listBoxLevels_SelectedIndexChanged);
+            // 
+            // labelLevels
+            // 
+            this.labelLevels.AutoSize = true;
+            this.labelLevels.Location = new System.Drawing.Point(854, 12);
+            this.labelLevels.Name = "labelLevels";
+            this.labelLevels.Size = new System.Drawing.Size(48, 13);
+            this.labelLevels.TabIndex = 5;
+            this.labelLevels.Text = "Уровни:";
+            // 
             // FormParking
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(984, 450);
+            this.Controls.Add(this.labelLevels);
+            this.Controls.Add(this.listBoxLevels);
             this.Controls.Add(this.groupBoxParking);
             this.Controls.Add(this.ParkingLocomotive);
             this.Controls.Add(this.ParkingMonorail);
             this.Controls.Add(this.pictureBoxParking);
             this.Name = "FormParking";
-            this.Text = "FormParking";
+            this.Text = "Депо";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxParking)).EndInit();
             this.groupBoxParking.ResumeLayout(false);
             this.groupBoxParking.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTake)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -145,5 +168,7 @@
         private System.Windows.Forms.Button buttonTake;
         private System.Windows.Forms.MaskedTextBox maskedTextBoxParking;
         private System.Windows.Forms.Label labelParking;
+        private System.Windows.Forms.ListBox listBoxLevels;
+        private System.Windows.Forms.Label labelLevels;
     }
 }
--- WindowsFormsMonorail/WindowsFormsMonorail/Parking.cs
+++ WindowsFormsMonorail/WindowsFormsMonorail/Parking.cs
@@ -12,7 +12,11 @@ namespace WindowsFormsMonorail
         /// <summary>
         /// Массив объектов, которые храним
         /// </summary>
-        private T[] _places;
+        private Dictionary<int, T> _places;
+        /// <summary>
+        /// Максимальное количество мест на парковке
+        /// </summary>
+        private int _maxCount;
         /// <summary>
         /// Ширина окна отрисовки
         /// </summary>
@@ -37,13 +41,10 @@ namespace WindowsFormsMonorail
         /// <param name="pictureHeight">Рамзер парковки - высота</param>
         public Parking(int sizes, int pictureWidth, int pictureHeight)
         {
-            _places = new T[sizes];
+            _maxCount = sizes;
+            _places = new Dictionary<int, T>();
             PictureWidth = pictureWidth;
             PictureHeight = pictureHeight;
-            for (int i = 0; i < _places.Length; i++)
-            {
-                _places[i] = null;
-            }
         }
         /// <summary>
         /// Перегрузка оператора сложения
@@ -54,11 +55,15 @@ namespace WindowsFormsMonorail
         /// <returns></returns>
         public static int operator +(Parking<T> p, T car)
         {
-            for (int i = 0; i < p._places.Length; i++)
+            if (p._places.Count == p._maxCount)
+            {
+                return -1;
+            }
+            for (int i = 0; i < p._maxCount; i++)
             {
                 if (p.CheckFreePlace(i))
                 {
-                    p._places[i] = car;
+                    p._places.Add(i, car);
                     p._places[i].SetPosition(5 + i / 5 * _placeSizeWidth + 5,
                      i % 5 * _placeSizeHeight + 15, p.PictureWidth,
                     p.PictureHeight);
@@ -67,47 +72,42 @@ namespace WindowsFormsMonorail
             }
             return -1;
         }
-
-
         public static T operator -(Parking<T> p, int index)
         {
-            if (index < 0 || index > p._places.Length)
-            {
-                return null;
-            }
             if (!p.CheckFreePlace(index))
             {
                 T car = p._places[index];
-                p._places[index] = null;
+                p._places.Remove(index);
                 return car;
             }
             return null;
         }
-
-
         private bool CheckFreePlace(int index)
         {
-            return _places[index] == null;
+            return !_places.ContainsKey(index);
         }
-
+        /// <summary>
+        /// Метод отрисовки парковки
+        /// </summary>
+        /// <param name="g"></param>
         public void Draw(Graphics g)
         {
             DrawMarking(g);
-            for (int i = 0; i < _places.Length; i++)
+            var keys = _places.Keys.ToList();
+            for (int i = 0; i < keys.Count; i++)
             {
-                if (!CheckFreePlace(i))
-                {//если место не пустое
-                    _places[i].DrawMonorail(g);
-                }
+                _places[keys[i]].DrawMonorail(g);
             }
         }
-
-
+        /// <summary>
+        /// Метод отрисовки разметки парковочных мест
+        /// </summary>
+        /// <param name="g"></param>
         private void DrawMarking(Graphics g)
         {
             Pen pen = new Pen(Color.Black, 3);
-            g.DrawRectangle(pen, 0, 0, (_places.Length / 5) * _placeSizeWidth, 480);
-            for (int i = 0; i < _places.Length / 5; i++)
+            g.DrawRectangle(pen, 0, 0, (_maxCount / 5) * _placeSizeWidth, 480);
+            for (int i = 0; i < _maxCount / 5; i++)
             {
                 for (int j = 0; j < 6; ++j)
                 {
